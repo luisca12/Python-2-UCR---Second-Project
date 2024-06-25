b1c6590b18e86c5e8c762281124c83e445bf9396
luisca12 <solera.luis12@gmail.com>
2024-06-24 21:50:19 -0600

    Added unitary tests

--- tests/test_httpRequests.py
+++ tests/test_httpRequests.py
@@ -5,7 +5,6 @@ import logging
 
 from src.functions import mkdir
 from src.log import logConfiguration
-from src.httpRequests import getToken, imageNumThread
 
 infoLog = logging.getLogger('infoLog')
 
@@ -14,14 +13,36 @@ class TestGetToken(unittest.TestCase):
     pass
 
 class TestimageNumThread(unittest.TestCase):
-    def test_DownloadImage(self):
+
+    def setUp(self):
         mkdir()
-        Token = getToken()
-        imageNumber = 4
-        imageNumThread(Token, imageNumber)
+        from src.httpRequests import getToken
+        self.imagePath = os.listdir('images')
+        self.imageName = os.path.exists('images/image_1.jpg')
+        self.Token = getToken()
+        self.imageNumber = 4
 
-        imageExists = os.path.exists('images/image_1.jpg')
-        self.assertTrue(imageExists)
+    def test_DownloadImage(self):
+        from src.httpRequests import imageNumThread
+        imageNumThread(self.Token, self.imageNumber)
 
-# if __name__ == '__main__':
-#     unittest.main()
+        self.assertTrue('images')
+    
+    def test_DownloadImage_2(self):
+        from src.httpRequests import imageNumThread
+        imageNumThread(self.Token, self.imageNumber)
+
+        self.assertTrue(self.imageName)
+
+    def test_DownloadImage_3(self):
+        from src.httpRequests import imageNumThread
+        imageNumThread(self.Token, self.imageNumber)
+
+        self.assertIn('image_1.jpg', self.imagePath)
+
+    def tearDown(self):
+        if os.path.exists('image_1.jpg') == True:
+            os.remove('image_1.jpg')
+
+if __name__ == '__main__':
+    unittest.main()
