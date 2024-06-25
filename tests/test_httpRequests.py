783360f3a8b4bcbbb329175c364360fecdb5553c
luisca12 <solera.luis12@gmail.com>
2024-06-24 22:11:42 -0600

    Updated unittest

--- tests/test_httpRequests.py
+++ tests/test_httpRequests.py
@@ -10,7 +10,29 @@ infoLog = logging.getLogger('infoLog')
 
 class TestGetToken(unittest.TestCase):
     
-    pass
+    def setUp(self):
+        if not os.path.exists('token'):
+            os.mkdir('token')
+            infoLog.info(f"The folder \"token\" does not exist. Creating folder \"token\".")
+
+    def test_GetToken(self):
+        from src.httpRequests import getToken
+        token = getToken()
+
+        self.assertIs(type(token), str)
+        infoLog.info(f"Running unittest: test_GetToken. The token is a string")
+
+    def test_GetTokenNone(self):
+        from src.httpRequests import getToken
+        token = getToken()
+
+        self.assertIsNone(token)
+        infoLog.info(f"Running unittest: test_GetTokenNone. The token is None")
+
+    def tearDown(self):
+        if os.path.exists('token') == True:
+            os.remove('token')
+            infoLog.info(f"The folder \"token\" does exist. Deleting folder \"token\".")
 
 class TestimageNumThread(unittest.TestCase):
 
@@ -27,22 +49,27 @@ class TestimageNumThread(unittest.TestCase):
         imageNumThread(self.Token, self.imageNumber)
 
         self.assertTrue('images')
+        infoLog.info(f"Running unittest: test_DownloadImage. The folder images exists.")
     
     def test_DownloadImage_2(self):
         from src.httpRequests import imageNumThread
         imageNumThread(self.Token, self.imageNumber)
 
         self.assertTrue(self.imageName)
+        infoLog.info(f"Running unittest: test_DownloadImage_2. The file \"images/image_1.jpg\" exists.")
 
     def test_DownloadImage_3(self):
         from src.httpRequests import imageNumThread
         imageNumThread(self.Token, self.imageNumber)
 
         self.assertIn('image_1.jpg', self.imagePath)
+        infoLog.info(f"Running unittest: test_DownloadImage_3. The file \"image_1.jpg\" exists.")
 
-    def tearDown(self):
-        if os.path.exists('image_1.jpg') == True:
-            os.remove('image_1.jpg')
+    def tear(self):
+        if os.path.exists('images') == True:
+            os.remove('images')
+            infoLog.info(f"The folder \"images\" does exist. Deleting folder \"images\".")
+            infoLog.info(f"Folder \"images\" and all other images were successfully deleted.")
 
 if __name__ == '__main__':
     unittest.main()
