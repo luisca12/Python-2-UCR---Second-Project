d449a3b65d629eb4e0182df08fb41780082eb6bc
luisca12 <solera.luis12@gmail.com>
2024-06-24 21:22:50 -0600

    Added unittest

--- tests/test_httpRequests.py
+++ tests/test_httpRequests.py
@@ -1,4 +1,27 @@
 import unittest
+import os
+import logging
 
+
+from src.functions import mkdir
+from src.log import logConfiguration
 from src.httpRequests import getToken, imageNumThread
 
+infoLog = logging.getLogger('infoLog')
+
+class TestGetToken(unittest.TestCase):
+    
+    pass
+
+class TestimageNumThread(unittest.TestCase):
+    def test_DownloadImage(self):
+        mkdir()
+        Token = getToken()
+        imageNumber = 4
+        imageNumThread(Token, imageNumber)
+
+        imageExists = os.path.exists('images/image_1.jpg')
+        self.assertTrue(imageExists)
+
+# if __name__ == '__main__':
+#     unittest.main()
